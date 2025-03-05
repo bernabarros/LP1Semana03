2a5cb489dc1e99ec04012d516deb2be1a7c0c0eb
bernabarros <bernardobarros.work@gmail.com>
2025-03-05 18:25:37 +0000

    Added ChangeString code

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,27 @@ namespace ChangeString
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string input_string;
+            string input_caracter;
+            string response_string = "";
+
+            Console.Write("Insere um string: ");
+            input_string = Console.ReadLine();
+            Console.Write("Insere um caracter: ");
+            input_caracter = Console.ReadLine();
+            char char_caracter = char.Parse(input_caracter);
+            foreach (char letter in input_string)
+            {
+                if (letter == char_caracter)
+                {
+                    response_string += "x";
+                }
+                else
+                {
+                    response_string += letter;
+                }
+            }
+            Console.WriteLine(response_string);
         }
     }
 }
