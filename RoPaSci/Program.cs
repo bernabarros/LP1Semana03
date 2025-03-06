efdc5921e769041bdad44804389750853a3f81bd
bernabarros <bernardobarros.work@gmail.com>
2025-03-06 22:13:51 +0000

    Replaced Program.cs from RoPaSci project

--- RoPaSci/Program.cs
+++ RoPaSci/Program.cs
@@ -2,11 +2,41 @@
 
 namespace RoPaSci
 {
-    public class Program
+    class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int result = RockPaperScissors(args[0], args[1]);
+            switch (result)
+            {
+                case 0:
+                    Console.WriteLine("It's a draw!");
+                    break;
+                case 1:
+                    Console.WriteLine("Player 1 wins!");
+                    break;
+                case 2:
+                    Console.WriteLine("Player 2 wins!");
+                    break;
+            }
+        }
+
+        private static int RockPaperScissors(string player1, string player2)
+        {
+            if (player1 == player2)
+            {
+                return 0; // Draw
+            }
+            if (((player1 == "Rock") && (player2 == "Scissors")) ||
+                ((player1 == "Scissors") && (player2 == "Paper")) ||
+                ((player1 == "Paper") && (player2 == "Rock")))
+            {
+                return 1; // Player 1 wins
+            }
+            else
+            {
+                return 2; // Player 2 wins
+            }
         }
     }
 }
